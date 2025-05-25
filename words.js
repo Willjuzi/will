3d2bf5d471b9d969449e5cfbad13be9bd7f256c2
Willjuzi <134662263+Willjuzi@users.noreply.github.com>
2025-05-26 04:32:44 +0800

    Update words.js

--- words.js
+++ words.js
@@ -1,14 +1,12 @@
-// words.js
 const wordList = [
   "over", "new", "art", "take", "only", "car", "park", "hard", "barn", "card",
-  "shark", "dark", "oh", "eyes", "date", "hold", "little", "work", "know", "place",
-  "years", "rain", "mail", "wait", "paint", "chant", "paid", "sail", "goods",
-  "services", "consumer", "producer", "live", "me", "cool", "give", "most", "saw",
-  "law", "raw", "jar", "straw", "draw", "country", "ocean", "title", "skip",
-  "change", "very", "after", "things", "our", "just", "girl", "dirt", "shirt",
-  "third", "thirst", "birth", "yellow", "add", "got", "life", "cycle", "pole",
-  "wire", "fight", "seen", "eat", "right", "might", "sight", "tight", "flight",
-  "bright", "lost", "far", "push", "pull", "resource", "say", "great", "four",
-  "help", "through", "gold", "cold", "fold", "mold", "sold", "told", "subtract",
-  "answer", "baby", "parent", "stages"
+  "shark", "dark", "eyes", "date", "hold", "little", "work", "know", "place", "years",
+  "rain", "mail", "wait", "paint", "chant", "paid", "sail", "goods", "services",
+  "consumer", "producer", "live", "cool", "give", "most", "saw", "law", "raw", "jar",
+  "straw", "draw", "country", "ocean", "title", "skip", "change", "very", "after",
+  "things", "our", "just", "girl", "dirt", "shirt", "third", "thirst", "birth",
+  "yellow", "add", "got", "life", "cycle", "pole", "wire", "fight", "seen", "eat",
+  "right", "might", "sight", "tight", "flight", "bright", "lost", "far", "push", "pull",
+  "resource", "say", "great", "four", "help", "through", "gold", "cold", "fold",
+  "mold", "sold", "told", "subtract", "answer", "baby", "parent", "stages"
 ];
